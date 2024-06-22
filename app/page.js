2ae28eae33e0043a48c3d0fd72b7193135d0136c
jduron24 <jduron24@iastate.edu>
2024-06-22 15:07:29 -0500

    t

--- app/page.js
+++ app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
         </div>
         <div className="deviceContainer">
           <Video className="screenRecording"  src={ScreenRecording}/>
-        </div> 
+        </div>  
       </div>
       <div className="savingCompanyContainer" >
         <div className="savingTimeContainer">
