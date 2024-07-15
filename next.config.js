2db7cf775ac20fac38222a0d882670d927158740
jduron24 <jduron24@iastate.edu>
2024-07-15 18:53:46 -0500

    fit all screens

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 
 import { withNextVideo } from 'next-video/process';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */ 
+const nextConfig = { 
+};
 
 export default withNextVideo(nextConfig)
